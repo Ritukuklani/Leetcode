6ae264ddae51c9c2f9f0b67a51ad2630f30ae837
Ritukuklani <rtkuklani@gmail.com>
2021-02-13 21:45:12 -0500

    Lonely pixel

---
+++ LonelyPixel1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        row_count = [0] * len(picture)
+        col_count = [0] * len(picture[0])
+        res = 0
+        for i in range(len(picture)):
+            for j in range(len(picture[0])):
+                if picture[i][j] == 'B':
+                    row_count[i] +=1
+                    col_count[j]+=1
+        for i in range(len(picture)):
+            for j in range(len(picture[0])):
+                if picture[i][j] == 'B' and row_count[i]==1 and col_count[j]==1:
+                    res+=1
+        return res
---
+++ TeemoAttacking.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        end = 0
+        res = 0
+        for i in range(len(timeSeries)):
+            if timeSeries[i]<end:
+                res += (timeSeries[i] + duration - end)
+                end = timeSeries[i] + duration
+            else:
+                res += (timeSeries[i] + duration - timeSeries[i])
+                end = timeSeries[i]+duration
+        return res  
